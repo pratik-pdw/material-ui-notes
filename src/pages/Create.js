86bd517e4f2130a6d2a944c1fa40b5e940664303
pratik-pdw <pratik.wadekar2810@gmail.com>
2021-04-09 12:02:33 +0530

    Typography done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+
+      <Button type="submit" color="primary">
+        Submit
+      </Button>
+      <Button variant="outlined" type="submit" color="primary">
+        Submit
+      </Button>
     </div>
-  )
+  );
 }
